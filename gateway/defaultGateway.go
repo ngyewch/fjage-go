75017af4a06172dd97b4c982a35fb5398ca9c894
Nick Ng <ngyewch@gmail.com>
2026-04-12 19:41:45 +0800

    refactor: message factory

--- gateway/defaultGateway.go
+++ gateway/defaultGateway.go
@@ -2,12 +2,10 @@ package gateway
 
 import (
 	"context"
-	"encoding/json"
 	"log/slog"
 
 	"github.com/google/uuid"
 	"github.com/ngyewch/fjage-go"
-	"github.com/ngyewch/fjage-go/services/shell"
 )
 
 type DefaultGateway struct {
@@ -272,43 +270,4 @@ func (gw *DefaultGateway) Send(ctx context.Context, message fjage.IMessage) (*Se
 	return &SendResponse{
 		Message: responseMessage,
 	}, nil
-}
-
-func instantiateMessage(clazz string) fjage.IMessage {
-	switch clazz {
-	case "org.arl.fjage.shell.GetFileReq":
-		return new(shell.GetFileReq)
-	case "org.arl.fjage.shell.GetFileRsp":
-		return new(shell.GetFileRsp)
-	case "org.arl.fjage.shell.PutFileReq":
-		return new(shell.PutFileReq)
-	case "org.arl.fjage.shell.ShellExecReq":
-		return new(shell.ShellExecReq)
-	case "org.arl.fjage.Message":
-		return new(fjage.Message)
-	default:
-		return nil
-	}
-}
-
-func unmarshalMessage(messageEnvelope *MessageEnvelope) (fjage.IMessage, error) {
-	if messageEnvelope == nil {
-		return nil, nil
-	}
-	jsonBytes, err := json.Marshal(messageEnvelope.Data)
-	if err != nil {
-		return nil, err
-	}
-	m := instantiateMessage(messageEnvelope.Clazz)
-	if m == nil {
-		m = new(fjage.Message)
-	}
-	if m == nil {
-		return nil, nil
-	}
-	err = json.Unmarshal(jsonBytes, m)
-	if err != nil {
-		return nil, err
-	}
-	return m, nil
 }
---
+++ gateway/factory.go
@@ -0,0 +1,47 @@
+package gateway
+
+import (
+	"encoding/json"
+
+	"github.com/ngyewch/fjage-go"
+	"github.com/ngyewch/fjage-go/services/shell"
+)
+
+func instantiateMessage(clazz string) fjage.IMessage {
+	switch clazz {
+	case "org.arl.fjage.shell.GetFileReq":
+		return new(shell.GetFileReq)
+	case "org.arl.fjage.shell.GetFileRsp":
+		return new(shell.GetFileRsp)
+	case "org.arl.fjage.shell.PutFileReq":
+		return new(shell.PutFileReq)
+	case "org.arl.fjage.shell.ShellExecReq":
+		return new(shell.ShellExecReq)
+	case "org.arl.fjage.Message":
+		return new(fjage.Message)
+	default:
+		return nil
+	}
+}
+
+func unmarshalMessage(messageEnvelope *MessageEnvelope) (fjage.IMessage, error) {
+	if messageEnvelope == nil {
+		return nil, nil
+	}
+	jsonBytes, err := json.Marshal(messageEnvelope.Data)
+	if err != nil {
+		return nil, err
+	}
+	m := instantiateMessage(messageEnvelope.Clazz)
+	if m == nil {
+		m = new(fjage.Message)
+	}
+	if m == nil {
+		return nil, nil
+	}
+	err = json.Unmarshal(jsonBytes, m)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}

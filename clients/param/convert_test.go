88df27289f58de6c48ffe0833328991871fe4994
Nick Ng <ngyewch@gmail.com>
2026-06-15 20:23:15 +0800

    feat: added converter

---
+++ clients/param/convert_test.go
@@ -0,0 +1,157 @@
+package param
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	epsilon = 0.0000001
+)
+
+func TestConvert(t *testing.T) {
+	doTestConvert(t, reflect.ValueOf(bool(true)), reflect.TypeFor[bool](), bool(true))
+	doTestConvert(t, reflect.ValueOf(string("abc")), reflect.TypeFor[string](), string("abc"))
+
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(int8(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(int16(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(int32(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(int64(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(uint8(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(uint16(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(uint32(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[float64](), float64(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[float32](), float32(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(uint64(123)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[float64](), float64(123.456))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[float32](), float32(123.456))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(float32(123.456)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[float64](), float64(123.456))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[float32](), float32(123.456))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[int64](), int64(123))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[int32](), int32(123))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[int16](), int16(123))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[int8](), int8(123))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[uint64](), uint64(123))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[uint32](), uint32(123))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[uint16](), uint16(123))
+	doTestConvert(t, reflect.ValueOf(float64(123.456)), reflect.TypeFor[uint8](), uint8(123))
+
+	doTestConvert(t, reflect.ValueOf([]int32{10, 20}), reflect.TypeFor[[]int32](), []int32{10, 20})
+	doTestConvert(t, reflect.ValueOf([]int32{10, 20}), reflect.TypeFor[[]int64](), []int64{10, 20})
+	doTestConvert(t, reflect.ValueOf([]any{int32(10), int64(20)}), reflect.TypeFor[[]int64](), []int64{10, 20})
+	doTestConvert(t, reflect.ValueOf([]any{float32(123.456), float64(-456.123)}), reflect.TypeFor[[]float64](), []float64{float64(123.456), float64(-456.123)})
+
+	doTestConvert(t,
+		reflect.ValueOf(map[string]any{
+			"foo": "bar",
+			"zoo": 123.456,
+		}),
+		reflect.TypeFor[map[string]any](),
+		map[string]any{
+			"foo": "bar",
+			"zoo": 123.456,
+		},
+	)
+}
+
+func doTestConvert(t *testing.T, sourceValue reflect.Value, targetType reflect.Type, expectedValue any) {
+	v, err := Convert(sourceValue, targetType)
+	if assert.NoError(t, err) {
+		if v.Kind() == reflect.Slice && v.Type().Elem().Kind() == reflect.Float64 {
+			assert.InEpsilonSlice(t, expectedValue, v.Interface(), epsilon)
+		} else if v.Kind() == reflect.Float64 {
+			assert.InEpsilon(t, expectedValue, v.Interface(), epsilon)
+		} else {
+			assert.Equal(t, expectedValue, v.Interface())
+		}
+	}
+}
---
+++ clients/param/converter.go
@@ -0,0 +1,30 @@
+package param
+
+import "reflect"
+
+func Convert(sourceValue reflect.Value, targetType reflect.Type) (reflect.Value, error) {
+	if sourceValue.Type().AssignableTo(targetType) {
+		return sourceValue, nil
+	} else if sourceValue.Type().ConvertibleTo(targetType) {
+		return sourceValue.Convert(targetType), nil
+	}
+	v := sourceValue.Interface()
+	sourceValue = reflect.ValueOf(v)
+	if sourceValue.Type().AssignableTo(targetType) {
+		return sourceValue, nil
+	} else if sourceValue.Type().ConvertibleTo(targetType) {
+		return sourceValue.Convert(targetType), nil
+	}
+	if (sourceValue.Kind() == reflect.Slice) && (targetType.Kind() == reflect.Slice) {
+		newSlice := reflect.MakeSlice(targetType, sourceValue.Len(), sourceValue.Cap())
+		for i := 0; i < sourceValue.Len(); i++ {
+			convertedValue, err := Convert(sourceValue.Index(i), targetType.Elem())
+			if err != nil {
+				return reflect.Value{}, err
+			}
+			newSlice.Index(i).Set(convertedValue)
+		}
+		return newSlice, nil
+	}
+	return reflect.Value{}, ErrCannotConvert
+}

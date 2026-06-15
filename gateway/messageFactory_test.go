f0aac1bfd910aea0f345b2a52b686a4c80e4c573
Nick Ng <ngyewch@gmail.com>
2026-06-15 19:09:12 +0800

    chore: updated tests

--- gateway/messageFactory_test.go
+++ gateway/messageFactory_test.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/goforj/godump"
+	"github.com/ngyewch/fjage-go"
+	"github.com/ngyewch/fjage-go/param"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,7 +18,25 @@ func Test1(t *testing.T) {
 	if assert.NoError(t, err) {
 		m, err := messageFactory.UnmarshalMessage(&messageEnvelope)
 		if assert.NoError(t, err) {
-			godump.Dump(m)
+			parameterRsp, ok := m.(*param.ParameterRsp)
+			if assert.True(t, ok) && assert.NotNil(t, parameterRsp) {
+				assert.Equal(t, "019d8595-09cf-7f9f-9715-66cda836c9c0", parameterRsp.MsgID)
+				assert.Equal(t, fjage.PerformativeInform, parameterRsp.Performative)
+				assert.Equal(t, "gateway-1adfee69-dbe1-4544-95c3-ac3b9fc12533", parameterRsp.Recipient)
+				assert.Equal(t, "device", parameterRsp.Sender)
+				assert.Equal(t, "c851cabf-9c22-4834-82a1-6740add5008f", parameterRsp.InReplyTo)
+				assert.Equal(t, int64(1776062630352), parameterRsp.SentAt)
+				assert.Equal(t, -1, parameterRsp.Index)
+				assert.Equal(t, "org.arl.unet.device.DeviceParam.health", parameterRsp.Param)
+				assert.EqualValues(t, []string{"org.arl.unet.device.DeviceParam.storage"}, parameterRsp.Readonly)
+				assert.EqualValues(t, &param.GenericValue{
+					Value: "OK",
+				}, parameterRsp.Value)
+				assert.EqualValues(t, map[string]*param.GenericValue{
+					"org.arl.unet.device.DeviceParam.storage": {Value: []any{float64(470695096320), float64(502116614144)}},
+					"org.arl.unet.device.DeviceParam.thermal": {Value: map[string]any{}},
+				}, parameterRsp.Values)
+			}
 		}
 	}
 }
